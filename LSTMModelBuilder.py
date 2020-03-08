542d305f0f141e90152a28b41d682e329126b3c7
duyanhpham@outlook.com <duyanhpham@outlook.com>
2020-03-08 16:25:22 +0100

    Changed LSTM dropout

--- LSTMModelBuilder.py
+++ LSTMModelBuilder.py
@@ -26,7 +26,7 @@ def build(data_shape_1, data_shape_2):
     model = keras.models.Sequential()
     model.add(keras.layers.LSTM(20,kernel_initializer='glorot_normal', return_sequences=True, input_shape=(data_shape_1,data_shape_2)))
     model.add(keras.layers.LSTM(20,kernel_initializer='glorot_normal', activation='selu'))
-    model.add(keras.layers.Dropout(0.5))
+    model.add(keras.layers.Dropout(0.3))
     model.add(keras.layers.Dense(data_shape_1,kernel_initializer='glorot_normal'))
     
     return model
